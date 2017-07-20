839b7a6a6ca87fd9a6e22c114f5456da9d0503ae
Nichols <nichols2@ELSLABM-155920.science.regn.net>
2017-07-20 12:51:09 +0100

    Adding website stuff

---
+++ dodge.js
@@ -0,0 +1,185 @@
+document.addEventListener('DOMContentLoaded',domloaded,false);
+function domloaded(){
+
+	var canvas = document.getElementById("myCanvas");
+	var button1 = document.getElementById('button1');
+	var refreshIntervalId;
+	var ctx = canvas.getContext("2d");
+	var cFont = ctx.font;
+	var fontArgs = ctx.font.split(' ');
+	ctx.font = '30px' + ' ' + fontArgs[fontArgs.length - 1]; /// using the last part
+
+	var squaresWidth = 25;
+	var x = canvas.width/2 - squaresWidth/2;
+	var y = canvas.height/2 - squaresWidth/2;
+	var dx = 4;
+	var dy = 4;
+
+	var rightPressed = false;
+	var leftPressed = false;
+	var upPressed = false;
+	var downPressed = false;
+
+	var attackers = [];
+	var attackersCount = 0;
+
+	function draw() {
+
+	    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+
+	     ctx.fillStyle = "grey";
+		 ctx.fillText(attackersCount,10,30);
+
+	    drawMe();
+
+	    drawAttackers();
+
+	    collisionDetection();
+
+	    moveMe();
+
+	    moveAttackers();
+
+
+
+	    
+
+
+	}
+
+	function moveAttackers(){
+
+		for(i=0; i<attackers.length; i++) {
+
+			if(attackers[i].x + attackers[i].dx + squaresWidth > canvas.width || attackers[i].x + attackers[i].dx < squaresWidth) {
+				    attackers[i].dx = -attackers[i].dx;
+			}
+
+			if(attackers[i].y + attackers[i].dy + squaresWidth > canvas.height || attackers[i].y + attackers[i].dy < squaresWidth) {
+				    attackers[i].dy = -attackers[i].dy;
+			}
+			
+			attackers[i].x += attackers[i].dx;
+			attackers[i].y += attackers[i].dy;
+		}
+	}
+
+	function drawAttackers() {
+		for(i=0; i<attackers.length; i++) {
+			ctx.beginPath();
+		    ctx.rect(attackers[i].x, attackers[i].y, squaresWidth, squaresWidth);
+		    ctx.fillStyle = attackers[i].color;
+		    ctx.fill();
+		    ctx.closePath();
+
+		}
+	}
+
+	function moveMe(){
+
+		if(rightPressed && x + squaresWidth < canvas.width) {
+		    x += dx;
+		}
+		
+		if(leftPressed && x>0) {
+		    x -= dx;
+		}
+
+		if (upPressed && y>0) {
+			y = y - dy;
+		}
+
+		if (downPressed && y + squaresWidth < canvas.height) {
+			y = y + dy;
+		}	
+	}
+
+	function drawMe() {
+	    ctx.beginPath();
+	    ctx.rect(x, y, squaresWidth, squaresWidth);
+	    ctx.fillStyle = "red";
+	    ctx.fill();
+	    ctx.closePath();
+	}
+
+	function spawnAttacker() {
+		var newSquare = {x: Math.random()*(canvas.width - squaresWidth/2), y: Math.random()*(canvas.height - squaresWidth/2), dx: Math.random(), dy: Math.random(), color: '#'+(Math.random()*0xFFFFFF<<0).toString(16)};
+		attackers.push(newSquare);
+		attackersCount++;
+	}
+
+
+	function keyDownHandler(e) {
+
+		switch(e.keyCode) {
+		    case 37:
+		        leftPressed = true;
+		        break;
+		    case 38:
+		        upPressed = true;
+		        break;
+		    case 39:
+		     	rightPressed = true;
+		     	break;
+		    case 40:
+		    	downPressed = true;
+		    	break;
+		    default:
+		        break;
+		}
+	}
+
+	function keyUpHandler(e) {
+		switch(e.keyCode) {
+		    case 37:
+		        leftPressed = false;
+		        break;
+		    case 38:
+		        upPressed = false;
+		        break;
+		    case 39:
+		     	rightPressed = false;
+		     	break;
+		    case 40:
+		    	downPressed = false;
+		    	break;
+		    default:
+		        break;
+		}
+	}
+
+	function collisionDetection() {
+		for(i=0; i<attackers.length; i++) {
+			if (x + squaresWidth >= attackers[i].x && x<attackers[i].x + squaresWidth && y + squaresWidth >= attackers[i].y && y < attackers[i].y + squaresWidth){ 
+				gameOver();
+			}
+		}
+	}
+
+	function gameOver() {
+				clearInterval(drawInterval);
+				clearInterval(spawnInterval);
+		        ctx.fillStyle = "red";
+		        ctx.textAlign = 'center';
+		        ctx.font = '40px' + ' ' + fontArgs[fontArgs.length - 1];
+				ctx.fillText("Game Over :(",canvas.width/2,canvas.height/2 -20);
+				ctx.fillStyle = 'grey';
+				ctx.font = '30px' + ' ' + fontArgs[fontArgs.length - 1];
+				ctx.fillText("Your score was " + attackersCount ,canvas.width/2,canvas.height/2 +20);
+				ctx.textAlign = 'start';
+	}
+
+
+	document.addEventListener("keydown", keyDownHandler, false);
+	document.addEventListener("keyup", keyUpHandler, false);
+	drawInterval = setInterval(draw, 10);
+	spawnInterval = setInterval(spawnAttacker, 1000);
+
+
+}
+
+function newGame(){
+		document.location.reload();
+		button1.style.display = 'none';
+}
